30773ac35241109f50db29e667cf26f0c1189f63
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-05-16 10:10:48 -0400

    Added Youtube::Video#entry to handle single entry vs. collection request #refactotum

--- lib/simple_youtube/youtube.rb
+++ lib/simple_youtube/youtube.rb
@@ -1,6 +1,13 @@
 #### Create classes for YouTube resources.
 module Youtube
   class Video < ActiveYoutube
+    def entry
+      if attributes.has_key?('entry')
+        return attributes['entry']
+      else
+        [self]
+      end
+    end
     def method_missing(method_symbol, *args)
       method_name = method_symbol.to_s
       return attributes[method_name] if attributes.include?(method_name)
