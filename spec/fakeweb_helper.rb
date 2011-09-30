db5bb9f93b93fa3dbb92a480a91e29311eeeac45
jamesshipton <ionysis@gmail.com>
2011-09-30 07:55:35 +0100

    add error stub

--- spec/fakeweb_helper.rb
+++ spec/fakeweb_helper.rb
@@ -2,7 +2,7 @@ require 'fakeweb'
 
 module FakewebHelper 
   # Make sure nothing gets out (IMPORTANT)  
-  FakeWeb.allow_net_connect = false  
+  FakeWeb.allow_net_connect = false
 
   # Turns a fixture file name into a full path  
   def fixture_file(filename)  
@@ -13,6 +13,10 @@ module FakewebHelper
   # Convenience methods for stubbing URLs to fixtures  
   def stub_get(url, filename)  
     FakeWeb.register_uri(:get, url, :response => fixture_file(filename))  
-  end  
+  end
+  
+  def stub_http_error(http_type, url, error_code, error_message)
+    FakeWeb.register_uri(http_type, url, :status => [error_code, error_message])  
+  end
 
 end
