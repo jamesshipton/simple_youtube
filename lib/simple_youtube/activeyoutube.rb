5727bab54f38e1197d72caa701e2100106f4764b
jamesshipton <ionysis@gmail.com>
2011-09-30 07:57:17 +0100

    upgrade to activeresource 3.1.0 explicitly specify xml format, as json now default

--- lib/simple_youtube/activeyoutube.rb
+++ lib/simple_youtube/activeyoutube.rb
@@ -3,16 +3,17 @@ require 'active_resource'
 require File.dirname(__FILE__) + "/entry_interface_shim"
 
 class ActiveYoutube < ActiveResource::Base
-  
+
   include EntryInterfaceShim
 
   self.site = "http://gdata.youtube.com/feeds/api"
+  self.format = :xml
 
   def self.find(args)
     scope, type, query = args[:scope], args[:type], args[:params]
     headers['Accept'] = "application/atom+xml"
     path = "#{prefix()}#{collection_name}#{'/' if scope}#{scope}#{'/' if type}#{type}#{query_string(query)}"
-    instantiate_record(connection.get(path, headers)) 
+    instantiate_record(format.decode(connection.get(path, headers).body))
   end  
-  
+
 end
