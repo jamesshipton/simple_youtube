883f8d9f266f24e35eed7b4884973dd4495d5f66
James Shipton <ionysis@gmail.com>
2013-03-14 07:50:08 +0000

    Update the updater to use oauth2

--- lib/simple_youtube/youtube.rb
+++ lib/simple_youtube/youtube.rb
@@ -2,15 +2,16 @@ require 'oauth'
 
 #### Create classes for YouTube resources.
 module Youtube
-
   class Video < ActiveYoutube
-
-    def self.update scope, user_name, update_xml, oauth_token, oauth_token_secret, x_gdata_key, host, host_secret
-      consumer = OAuth::Consumer.new(host, host_secret, {:site=>"https://www.google.com", :request_token_path=>"/accounts/OAuthGetRequestToken", :authorize_path=>"/accounts/OAuthAuthorizeToken", :access_token_path=>"/accounts/OAuthGetAccessToken"})
-      access_token = OAuth::AccessToken.new(consumer, oauth_token, oauth_token_secret)
-      access_token.put("http://gdata.youtube.com/feeds/api/users/#{user_name}/uploads/#{scope}", update_xml, {'Accept' => 'application/atom+xml', 'Content-Type' => 'application/atom+xml', 'X-GData-Key' => "key=#{x_gdata_key}" })
+    def self.update scope, user_name, update_xml, oauth_token, x_gdata_key
+      RestClient.put(
+        "http://gdata.youtube.com/feeds/api/users/#{user_name}/uploads/#{scope}",
+        update_xml,
+        { 'Authorization' => "Bearer #{oauth_token}",
+          'Content-Type'  => 'application/atom+xml',
+          'GData-Version' => '2',
+          'X-GData-Key'   => "key=#{x_gdata_key}" })
     end
-
   end
 
   class User < ActiveYoutube
@@ -21,5 +22,4 @@ module Youtube
 
   class Playlist < ActiveYoutube
   end
-
 end
--- spec/youtube_update_spec.rb
+++ spec/youtube_update_spec.rb
@@ -2,49 +2,29 @@ require 'spec_helper'
 
 describe 'YoutubeUpdateSpec' do
   it 'updates a video on youtube' do
-    consumer = double('consumer')
+
     access_token = double('access_token')
-    host = 'www.host.com'
-    host_secret = 'host_secret'
     oauth_token = 'oauth_token'
-    oauth_token_secret = "oauth_token_secret"
     x_gdata_key = 'x_gdata_key'
     video_uid = 'video_uid'
     user_name = 'user_name'
     update_xml = IO.read(SimpleYoutube::ROOT + '/spec/fixture/video_update.xml')
 
-    OAuth::Consumer.should_receive(:new).
-      with(
-        host,
-        host_secret,
-        { :site => 'https://www.google.com',
-          :request_token_path => '/accounts/OAuthGetRequestToken',
-          :authorize_path => '/accounts/OAuthAuthorizeToken',
-          :access_token_path => '/accounts/OAuthGetAccessToken' }).
-      and_return(consumer)
-
-    OAuth::AccessToken.should_receive(:new).
-      with(consumer, oauth_token, oauth_token_secret).
-      and_return(access_token)
-
-    access_token.should_receive(:put).
-      with(
+    RestClient.should_receive(:put).with(
         "http://gdata.youtube.com/feeds/api/users/#{user_name}/uploads/#{video_uid}",
         update_xml,
-        { 'Accept' => 'application/atom+xml',
-          'Content-Type' => 'application/atom+xml',
-          'X-GData-Key' => "key=#{x_gdata_key}" }).
-      and_return(Net::HTTPResponse.new(1.0, 200, 'OK'))
+        { 'Authorization' => "Bearer #{oauth_token}",
+          'Content-Type'  => 'application/atom+xml',
+          'GData-Version' => '2',
+          'X-GData-Key'   => "key=#{x_gdata_key}" }).
+      and_return(Net::HTTPResponse.new(1.1, 200, 'OK'))
 
     response = Youtube::Video.update(
       video_uid,
       user_name,
       update_xml,
       oauth_token,
-      oauth_token_secret,
-      x_gdata_key,
-      host,
-      host_secret)
+      x_gdata_key)
 
     response.code.should == 200
   end
